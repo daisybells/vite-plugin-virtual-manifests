f055c00c65db78dd6a4754a0350d8f0a1b12c8ae
gwen <cubinggamer@gmail.com>
2025-09-23 14:56:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,167 @@
+# vite-plugin-virtual-manifests
+
+A zero-dependency Vite plugin that generates JSON manifests based on the contents of a specified file path or directory.
+
+This plugin is useful for creating dynamic manifests, such as asset lists or configuration files, that are automatically updated during development and cached on disk during build process.
+
+## Features
+
+-   **Live Updating File Lists**: Automatically list files within an array.
+-   **Virtual Module Imports**: Import generated data directly from a virtual environment as opposed to reading `.json` files on disk.
+-   **Transform Manifests Based on Apply Context**: Edit the manifest depending on if you are in your dev server or building the manifest (e.g. if `.png` files convert to `.webp` files during build).
+
+## Installation
+
+```
+# Using npm
+npm install vite-plugin-virtual-manifests --save-dev
+```
+
+```
+# Using yarn
+yarn add vite-plugin-virtual-manifests --dev
+```
+
+```
+# Using pnpm
+pnpm add -D vite-plugin-virtual-manifests
+```
+
+## Basic Usage
+
+1. Add the plugin to your `vite.config.js` or `vite.config.ts`.
+
+2. Configure a `manifests` array with a `name`, a `generate` function, and a `watchDir`.
+
+`vite.config.js`
+
+```
+import { defineConfig } from 'vite';
+import viteManifestBuilder from 'vite-plugin-manifest-builder';
+import path from 'node:path';
+import fs from "node:fs/promises"
+
+// Example generator function
+async function generateAssetManifest(directory) {
+  const assets = [];
+  const files = await fs.readdir(directory);
+
+  for (const file of files) {
+    assets.push({
+      path: path.join(directory, file),
+      name: file,
+    });
+  }
+  return assets;
+}
+
+export default defineConfig({
+  plugins: [
+    viteManifestBuilder({
+      manifests: [
+        {
+          name: 'assets',
+          watchDir: 'public/assets',
+          generate: generateAssetManifest,
+        },
+      ],
+    }),
+  ],
+});
+```
+
+### Using the Manifest
+
+You can then import the generated manifest as a virtual module within your application code:
+
+```
+import assetsManifest from `virtual:manifests/assets`;
+console.log(assetsManifest);
+// Output will be the JSON data returned by your generator function.
+```
+
+## Configuration Options
+
+The plugin is configured with a single `options` object.
+
+`viteVirtualManifests(options)`
+
+| **Option**        | **Type**        | **Default**                             | **Description**                                                |
+| ----------------- | --------------- | --------------------------------------- | -------------------------------------------------------------- |
+| `outputDirectory` | `string`        | `path.join(proces.cwd(), ".manifests")` | The directory where manifests will be cached to disk.          |
+| `cacheManifests`  | `boolean`       | `true`                                  | When `true`, manifests are saved to disk during build process. |
+| `manifests`       | `Array<Object>` | `(required)`                            | An array of manifest configurations.                           |
+
+`options.manifests`
+
+**Type**: `Array<Object>`
+**Required**
+
+An array of manifest configuration objects.
+
+| **Option**    | **Type**                  | **Default**  | **Description**                                                                                                                                |
+| ------------- | ------------------------- | ------------ | ---------------------------------------------------------------------------------------------------------------------------------------------- |
+| `name`        | `string`                  | `(required)` | The name of the manifest, used for virtual module imports (e.g., `virtual:manifests/<name>`)                                                   |
+| `watchDir`    | `string`                  | `null`       | The directory to watch for changes. Changes will trigger a manifest regeneration and a full page reload in dev mode.                           |
+| `generate`    | `Function`                | `(required)` | A callback function that revieves the watchDir and returns the JSON data to be written to the manifest. It can be synchronous or asynchronous. |
+| `watchIgnore` | `string \| Array<string>` | `[]`         | A file path or array of file paths to ignore when watching for changes. Useful for ignoring specific files within a watched directory.         |
+| `transform`   | `Object`                  | `null`       | Options instrunctions to transform the manifest's JSON data.                                                                                   |
+
+`options.manifests[].transform`
+
+**Type**: `Object`
+
+JSON Transform configuration
+
+| **Option**      | **Type**             | **Default**  | **Description**                                                                                                                                                  |
+| --------------- | -------------------- | ------------ | ---------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| `apply`         | `"serve" \| "build"` | `(optional)` | Defines when the transform function should be applied. If omitted, the transform will be applied in both development (`serve`) and build (`build`) environments. |
+| `jsonTransform` | `Function`           | `(optional)` | A callback function that receives the generated JSON data and returns the transformed data.                                                                      |
+
+### Example Transform
+
+```
+// vite.config.js
+import { defineConfig } from "vite";
+import viteVirtualManifests from "vite-plugin-virtual-manifests"
+import fs from "node:fs/promises"
+import path from "node:path";
+
+async function generateImageManifest(directory) {
+  const images = await fs.readdir(directory, { withFileTypes: true });
+  return images
+    .filter(fileData => fileData.isFile() && [".png", ".jpg", ".jpeg"].includes(path.extname(fileData.name.toLowerCase())))
+    .map(fileData => ({
+      name: fileData.name,
+      path: `${path.join(path.basename(directory), fileData.name)},
+    }))
+}
+
+function changePngToWebp(jsonData) {
+  return jsonData.map(image => {
+    return {...image, webpPath: image.path.replace(/\.(png|jpg|jpeg)/giu, ".webp")}
+  })
+}
+
+export default defineConfig({
+  plugins: [
+    viteVirtualManifests({
+      manifests: [
+        {
+          name: "images",
+          watchDir: path.resolve(__dirname, "public/images"),
+          generate: generateImageManifest,
+          transform: {
+            apply: "build",
+            jsonTransform: changePngToWebp
+          }
+        }
+      ]
+    })
+  ]
+})
+```
+
+## License
+
+This project is licensed under the MIT License.
---
+++ index.js
@@ -0,0 +1,237 @@
+import path from "node:path";
+import fs from "node:fs/promises";
+
+const VIRTUAL_MODULE_PREFIX = "virtual:manifests/";
+const RESOLVED_VIRTUAL_MODULE_PREFIX = `\0${VIRTUAL_MODULE_PREFIX}`;
+
+const defaultOptions = {
+    outputDirectory: path.join(process.cwd(), ".manifests"),
+    cacheManifests: true,
+};
+
+/**
+ * Callback function that generates JSON data.
+ * @callback generator
+ * @param {string} directory - Directory to be analyzed for manifest generation.
+ * @returns {Object|Array} - JSON object to be written to the manifest file.
+ *
+ */
+
+/**
+ * Callack function to handle the manifest.json transform.
+ * @callback transformFunction
+ * @param {Object|Array} JSONData - Input JSON data to be transformed.
+ * @returns {Object|Array} - Processed JSON data.
+ */
+
+/**
+ *
+ * Vite plugin that generates JSON manifests based off of the contents of an input
+ * file path.
+ * @param {Object} inputOptions - Configurable options for manifest generator.
+ * @param {string} [inputOptions.outputDirectory = path.join(process.cwd(), ".manifests")] - Directory to save
+ * manifests into for external use.
+ * @param {Boolean} [inputOptions.cacheManifests = true] - Defines whether or not to save manifests directly to disk.
+ * @param {Object[]} inputOptions.manifests - List of manifest files set to be generated as well as
+ * their configuration.
+ * @param {generator} inputOptions.manifests[].generate - Callback function that generates JSON data
+ * from the input file path to be written to the manifest.
+ *
+ * @param {string} inputOptions.manifests[].name - Identification name for logs and error messages.
+ * @param {string} inputOptions.manifests[].watchDir - directory to watch for changes to update
+ * manifest during development.
+ *
+ * @param {Object} [inputOptions.manifests[].transform] - Instructions to transform module at either "serve" or "build"
+ * time. Useful for filetype conversions at build time (ex. converting .png files to .webp).
+ * @param {"serve" | "build"} [inputOptions.manifests[].transform.apply] - Define whether to apply module transform at
+ * server time or build time.
+ * @param {transformFunction} [inputOptions.manifests[].transform.jsonTransform] - Callback function that defines how
+ * the JSON data should be transformed.
+ *
+ *
+ * @returns {import("vite").Plugin} - Vite plugin object.
+ */
+
+function viteVirtualManifests(inputOptions = {}) {
+    const options = {
+        ...defaultOptions,
+        ...inputOptions,
+    };
+    const { manifests, outputDirectory, cacheManifests } = options;
+
+    const manifestArray = Array.isArray(manifests) ? manifests : [];
+
+    const resolvedManifests = manifestArray.map((manifest) => {
+        const { watchDir, watchIgnore, name } = manifest;
+        const ignores = watchIgnore || [];
+        const ignoresArray = Array.isArray(ignores) ? ignores : [ignores];
+
+        return {
+            ...manifest,
+            watchDir: watchDir ? path.resolve(watchDir) : null,
+            watchIgnore: ignoresArray.map((filePath) => path.resolve(filePath)),
+            output: path.resolve(outputDirectory, `${name}.json`),
+        };
+    });
+
+    let command;
+
+    return {
+        name: "vite-plugin-virtual-manifests",
+        enforce: "pre",
+        configResolved(config) {
+            ({ command } = config);
+        },
+        resolveId(id) {
+            if (id.startsWith(VIRTUAL_MODULE_PREFIX))
+                return `${RESOLVED_VIRTUAL_MODULE_PREFIX}${id.slice(
+                    VIRTUAL_MODULE_PREFIX.length
+                )}`;
+            return null;
+        },
+        async load(id) {
+            if (id.startsWith(RESOLVED_VIRTUAL_MODULE_PREFIX)) {
+                const name = id.slice(RESOLVED_VIRTUAL_MODULE_PREFIX.length);
+
+                const currentManifest = resolvedManifests.find(
+                    (manifest) => manifest.name === name
+                );
+                if (!currentManifest)
+                    this.error(
+                        `Manifest with name "${name}" not found for virtual import`
+                    );
+
+                const { generate, watchDir, transform } = currentManifest;
+
+                const data = await Promise.resolve(generate(watchDir));
+
+                if (transform) {
+                    const transformedData = transformData(
+                        data,
+                        transform,
+                        command
+                    );
+
+                    return `export default ${JSON.stringify(
+                        transformedData,
+                        null,
+                        2
+                    )};`;
+                }
+
+                return `export default ${JSON.stringify(data, null, 2)};`;
+            }
+            return null;
+        },
+
+        async buildStart() {
+            if (cacheManifests)
+                await Promise.all(resolvedManifests.map(generateManifestCache));
+        },
+        configureServer(server) {
+            const { root, publicDir } = server.config;
+            const watchDirectories = resolvedManifests
+                .map((manifest) => manifest.watchDir)
+                .filter(Boolean)
+                .toSorted();
+
+            const watchDirectorySet = [...new Set(watchDirectories)];
+
+            if (watchDirectorySet.length === 0) return;
+
+            const topLevelDirectories = returnTopLevelPaths(watchDirectorySet);
+
+            for (const directory of topLevelDirectories) {
+                const isWatched =
+                    directory.startsWith(publicDir) ||
+                    directory.startsWith(root);
+                if (!isWatched) {
+                    console.log("Adding watcher");
+                    server.watcher.add(directory);
+                }
+            }
+
+            const handleFileChange = handleFileChangeCurry(
+                server,
+                resolvedManifests
+            );
+
+            server.watcher.on("add", handleFileChange);
+            server.watcher.on("change", handleFileChange);
+            server.watcher.on("unlink", handleFileChange);
+        },
+    };
+}
+
+function transformData(data, transformOptions, command) {
+    const { jsonTransform, apply } = transformOptions;
+    const shouldApply = apply === command || !apply;
+
+    if (!jsonTransform || !shouldApply) return data;
+
+    console.log(`Applying transform for "${command}" time`);
+
+    return jsonTransform(data);
+}
+
+function handleFileChangeCurry(server, manifests) {
+    return (filePath) => {
+        for (const manifest of manifests) {
+            const isIgnored = manifest.watchIgnore.some((ignoredPath) =>
+                filePath.startsWith(ignoredPath)
+            );
+            const isWithinDirectory = filePath.startsWith(manifest.watchDir);
+
+            if (!isWithinDirectory || isIgnored) continue;
+
+            console.log(
+                `\n Change detected in ${manifest.name}. Regenerating...`
+            );
+
+            const resolveId = `${RESOLVED_VIRTUAL_MODULE_PREFIX}${manifest.name}`;
+            const module = server.moduleGraph.getModuleById(resolveId);
+            if (!module) continue;
+
+            server.moduleGraph.invalidateModule(module);
+            server.ws.send({ type: "full-reload", path: "*" });
+            console.log(
+                `\n♻️ Virtual module "${VIRTUAL_MODULE_PREFIX}${manifest.name}" invalidated. Page will reload.`
+            );
+            return;
+        }
+    };
+}
+
+async function generateManifestCache(manifest) {
+    const { name, watchDir, generate, output } = manifest;
+
+    try {
+        const data = await generate(watchDir);
+        await fs.mkdir(path.dirname(output), { recursive: true });
+        await fs.writeFile(output, JSON.stringify(data, null, 2));
+        console.log(`\n ✅ ${name} manifest written to ${output}`);
+    } catch (error) {
+        console.error(`\n ❌ Error generating manifest [${name}]: ${error}`);
+    }
+}
+
+function returnTopLevelPaths(filePathArray) {
+    const sortedArray = filePathArray.toSorted();
+
+    const topLevelDirectories = sortedArray.reduce(
+        (accumulator, currentValue) => {
+            const lastAddedDirectory = accumulator.at(-1);
+            if (
+                lastAddedDirectory &&
+                currentValue.startsWith(`${lastAddedDirectory}${path.sep}`)
+            )
+                return accumulator;
+
+            return [...accumulator, currentValue];
+        },
+        []
+    );
+    return topLevelDirectories;
+}
+
+export default viteVirtualManifests;
